3d80c0699ecca5cdc4f0ce77928744a4a81604ad
Saket Choudhary <saketkc@gmail.com>
2015-02-06 15:46:06 -0800

    New test cases

---
+++ tests/test_fasta_reader.cpp
@@ -0,0 +1,18 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "fasta_reader.cpp"
+
+class FastaReaderTestFixture{
+    public:
+        static char input_file[];
+        int numberOfSequences(){
+            std::vector<Fasta> fasta_sequences;
+            fasta_sequences = FastaReader(input_file);
+            return fasta_sequences.size();
+        }
+};
+
+char FastaReaderTestFixture::input_file[] = "./data/multiple_seq3.fasta";
+TEST_CASE_METHOD(FastaReaderTestFixture, "Test Fasta Reader", "[create]"){
+    REQUIRE(numberOfSequences()==3);
+}
